9c4af45342f082a7917b7c08c419f60334dec394
Sagar paul <31154843+Andrewrick1@users.noreply.github.com>
2020-10-02 00:08:18 +0530

    Update palindrome.rb

--- Day2/Ruby/palindrome.rb
+++ Day2/Ruby/palindrome.rb
@@ -2,8 +2,10 @@
  @author: aaditkamat
  @date: 22/12/2018
 =end
-require './reverse'
-
-def solution(str)
-  str != nil and str === short_solution(str)
+def palindrome?(string)
+ if string == string.reverse
+   return true
+ else
+   puts “String is not a palindrome”
+ end
 end
